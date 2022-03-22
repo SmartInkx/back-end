7733ae235d3f55cd3f88aa028c07c98e25a94a30
Rodrigo Pereira <rodrigopalexandre1@hotmail.com>
2022-03-21 21:48:31 -0300

    Adicionando pasta projeto

---
+++ smartink/src/main/java/com/oasis/smartink/SmartinkApplication.java
@@ -0,0 +1,13 @@
+package com.oasis.smartink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartinkApplication.class, args);
+	}
+
+}
---
+++ smartink/src/test/java/com/oasis/smartink/SmartinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasis.smartink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
