d1ed9f94d1b7f81e351667dc9520b6b22ab173b2
renanmeli <renan.rsantana@mercadolivre.com>
2022-04-07 00:36:12 -0300

    Adicionando funcionalidade de subir imagem

--- src/main/java/com/oasis/smartink/service/FilesService.java
+++ src/main/java/com/oasis/smartink/service/FilesService.java
@@ -1,8 +1,10 @@
 package com.oasis.smartink.service;
 
 import com.oasis.smartink.model.Files;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Service
 public interface FilesService {
 
     Files save(MultipartFile files, String idUser);
